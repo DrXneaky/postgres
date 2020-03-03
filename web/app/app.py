22b723b072079c0f2e2f925c075c08a6de41f8ab
DrXneaky <33802435+DrXneaky@users.noreply.github.com>
2020-03-03 12:14:33 +0100

    postgres added (up for test)

--- web/app/app.py
+++ web/app/app.py
@@ -15,10 +15,29 @@
 
 
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object("config.Config")
+db = flask_sqlalchemy(app)
+create_db()
+ 
+class User(db.Model):
+    __tablename__ = "users"
 
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, email):
+        self.email = email
+
+
+
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 
 @app.route('/')
